aa6ac28e08f1997b4a74f9fcb7eb13b519989e37
dove <dove0012@163.com>
2019-10-21 20:19:46 +0800

    修改日志包方法

--- common/utils/log/log.go
+++ common/utils/log/log.go
@@ -1,36 +1,64 @@
 package log
 
 import (
-	"gowork/common/utils/time"
+	"gserver/common/utils/time"
 	l "log"
 	"runtime"
 )
 
-func Error2Exit(err error, msg string) {
-	if err != nil {
-		l.Printf("%s: %s\n", msg, err)
-		runtime.Goexit()
+//Info log info
+func Info(msg string, v ...interface{}) {
+	str := "log info: " + msg + "\n"
+	if v != nil {
+		l.Printf(str, v)
+	} else {
+		l.Printf(str)
+	}
+
+}
+
+//Warn log warn
+func Warn(msg string, v ...interface{}) {
+	str := "log warning: " + msg + "\n"
+	if v != nil {
+		l.Printf(str, v)
+	} else {
+		l.Printf(str)
 	}
 }
 
-func Info(msg string) {
-	l.Printf("log info: %s\n", msg)
-}
-
-func Warn(msg string) {
-	l.Printf("log warning: %s\n", msg)
-}
-
-func Debug(msg string) {
-	l.Printf("log debug: %s\n", msg)
-}
-
-func Error(err error, msg string) {
-	if err != nil {
-		l.Printf("%s: %s\n", msg, err)
+//Debug log debug
+func Debug(msg string, v ...interface{}) {
+	str := "log debug: " + msg + "\n"
+	if v != nil {
+		l.Printf(str, v)
+	} else {
+		l.Printf(str)
 	}
 }
 
+//Error log error
+func Error(msg string, v ...interface{}) {
+	str := "log Error: " + msg + "\n"
+	if v != nil {
+		l.Printf(str, v)
+	} else {
+		l.Printf(str)
+	}
+}
+
+//Error2Exit log error2Exit
+func Error2Exit(msg string, v ...interface{}) {
+	str := "log Error2Exit: " + msg + "\n"
+	if v != nil {
+		l.Printf(str, v)
+	} else {
+		l.Printf(str)
+	}
+	runtime.Goexit()
+}
+
+//TimeConsuming TimeConsuming
 func TimeConsuming(starTime int64, msg string) {
-	l.Printf("%s TimeConsuming: %f second\n", msg, float32(time.NowUnixMilli() - starTime) / 1000)
+	l.Printf("%s TimeConsuming: %f second\n", msg, float32(time.NowUnixMilli()-starTime)/1000)
 }
